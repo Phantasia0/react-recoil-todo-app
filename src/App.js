83a2d57ae90328acdc048fded302a335de3dbfa5
Phantasia0 <ncprog1@gmail.com>
2023-04-05 08:49:36 +0900

    recoil-todo-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRecoilValue } from "recoil";
+import "./App.css";
+import TodoItemCreator from "./components/TodoItemCreator";
+import { filteredTodoListState } from "./todoAtom";
+import TodoItem from "./components/TodoItem";
+import TodoListFilters from "./components/TodoListFilters";
+import TodoListStats from "./components/TodoListStats";
+import { currentUserNameQuery } from "./userAtom";
+import { Suspense } from "react";
 
 function App() {
+  const todoList = useRecoilValue(filteredTodoListState);
+  console.log(todoList);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<div>Loading...</div>}>
+        <CurrentUserInfo />
+      </Suspense>
+      <TodoListStats />
+      <TodoListFilters />
+      <TodoItemCreator />
+      {todoList.map((todoItem) => (
+        <TodoItem key={todoItem.id} item={todoItem} />
+      ))}
     </div>
   );
 }
 
 export default App;
+
+const CurrentUserInfo = () => {
+  const userName = useRecoilValue(currentUserNameQuery);
+  return <div>{userName}</div>;
+};
